c988f49dd1d0ecd431206960ae7db875f1bca52b
Lucia Bazan-Williamson <jbazanwilliamson@ucmerced.edu>
2024-12-20 16:26:13 -0800

    Updates on folders such as scripts, plot and data

---
+++ scripts/pca_plot_function.py
@@ -0,0 +1,291 @@
+def define_font_properties():
+    import matplotlib
+    """
+    Defines font properties.
+    arguments:
+    raises:
+    returns:
+        (dict<object>): references to definitions of font properties
+    """
+# Define font values.
+    values_one = {
+"family": "sans-serif",
+"style": "normal",
+"variant": "normal",
+"stretch": 1000,
+"weight": 1000,
+"size": 30
+    }
+    values_two = {
+"family": "sans-serif",
+"style": "normal",
+"variant": "normal",
+"stretch": 500,
+"weight": 1000,
+"size": 25
+    }
+    values_three = {
+"family": "sans-serif",
+"style": "normal",
+"variant": "normal",
+"stretch": 500,
+"weight": 1000,
+"size": 20
+    }
+    values_four = {
+"family": "sans-serif",
+"style": "normal",
+"variant": "normal",
+"stretch": 500,
+"weight": 500,
+"size": 15
+    }
+# Define font properties.
+    properties_one = matplotlib.font_manager.FontProperties(
+family=values_one["family"],
+style=values_one["style"],
+variant=values_one["variant"],
+stretch=values_one["stretch"],
+weight=values_one["weight"],
+size=values_one["size"]
+    )
+    properties_two = matplotlib.font_manager.FontProperties(
+family=values_two["family"],
+style=values_two["style"],
+variant=values_two["variant"],
+stretch=values_two["stretch"],
+weight=values_two["weight"],
+size=values_two["size"]
+    )
+    properties_three = matplotlib.font_manager.FontProperties(
+family=values_three["family"],
+style=values_three["style"],
+variant=values_three["variant"],
+stretch=values_three["stretch"],
+weight=values_three["weight"],
+size=values_three["size"]
+    )
+    properties_four = matplotlib.font_manager.FontProperties(
+family=values_four["family"],
+style=values_four["style"],
+variant=values_four["variant"],
+stretch=values_four["stretch"],
+weight=values_four["weight"],
+size=values_four["size"]
+    )
+# Compile and return references.
+    return {
+"values": {
+"one": values_one,
+"two": values_two,
+"three": values_three,
+"four": values_four
+        },
+"properties": {
+"one": properties_one,
+"two": properties_two,
+"three": properties_three,
+"four": properties_four
+        }
+    }
+
+
+
+def define_color_properties():
+    import seaborn
+    import matplotlib
+    
+    
+    """
+    Defines color properties.
+    arguments:
+    raises:
+    returns:
+        (dict<tuple>): references to definitions of color properties
+    """
+# Black.
+    black = (0.0, 0.0, 0.0, 1.0)
+# White.
+    white = (1.0, 1.0, 1.0, 1.0)
+    white_faint = (1.0, 1.0, 1.0, 0.75)
+# Blue.
+    blue = (0.0, 0.2, 0.5, 1.0)
+    blue_faint = (0.0, 0.2, 0.5, 0.75)
+# Orange.
+    orange = (1.0, 0.6, 0.2, 1.0)
+    orange_faint = (1.0, 0.6, 0.2, 0.75)
+# Compile and return references.
+    return {
+"black": black,
+"white": white,
+"white_faint": white_faint,
+"blue": blue,
+"blue_faint": blue_faint,
+"orange": orange,
+"orange_faint": orange_faint
+    }
+
+
+def create_legend_elements(
+colors=None,
+labels=None,
+):
+    """
+    Creates custom elements for legend.
+    arguments:
+        colors (list<dict>): colors
+        labels (str): name of data column with independent variable
+    raises:
+    returns:
+        (list<object>): elements for legend
+    """
+    import seaborn
+    elements = []
+    for index in range(len(labels)):
+        element = matplotlib.lines.Line2D(
+            [0],
+            [0],
+            marker="o",
+            color=colors[index],
+            label=labels[index],
+            markerfacecolor=colors[index],
+            markersize=15,
+        )
+        elements.append(element)
+    return elements
+
+def plot_scatter_cluster_shape(
+data=None,
+abscissa=None,
+ordinate=None,
+label_horizontal=None,
+label_vertical=None,
+factor=None,
+fonts=None,
+colors=None,
+legend=None,
+):
+    """
+    Creates a figure of a chart of type histogram to represent the frequency
+    distribution of a single series of values.
+    arguments:
+        data (object): Pandas data frame of groups, series, and values
+        abscissa (str): name of data column with independent variable
+        ordinate (str): name of data column with dependent variable
+        label_horizontal (str): label for horizontal axis
+        label_vertical (str): label for vertical axis
+        factor (str): name of data column with groups or factors of samples
+        fonts (dict<object>): references to definitions of font properties
+        colors (dict<tuple>): references to definitions of color properties
+        legend (bool): whether to include a legend for series on the chart
+    raises:
+    returns:
+        (object): figure object
+    """
+##########
+# Organize data.
+# Separate points by groups.
+# Define groups.
+    
+    import matplotlib
+    data = data.copy(deep=True)   
+    data.set_index(
+        factor,
+append=False,
+drop=True,
+inplace=True
+    )
+    print(data)
+    groups = data.groupby(level=[factor])
+    print("Count of groups by factor: " + str(len(groups)))
+    import seaborn
+    colors_series = list(seaborn.color_palette("Set3", n_colors=len(groups)))
+    shape_series = list(['o','v','d','>','h','<','s'])
+##########
+# Create figure.
+    figure = matplotlib.pyplot.figure(
+    figsize=(15.748, 11.811),
+    tight_layout=True
+    )
+# Create axes.
+    axes = matplotlib.pyplot.axes()
+    axes.set_xlabel(
+    xlabel=label_horizontal,
+labelpad=20,
+alpha=1.0,
+backgroundcolor=colors["white"],
+color=colors["black"],
+fontproperties=fonts["properties"]["one"]
+    )
+    axes.set_ylabel(
+        ylabel=label_vertical,
+        labelpad=20,
+        alpha=1.0,
+        backgroundcolor=colors["white"],
+        color=colors["black"],
+        fontproperties=fonts["properties"]["one"]
+    )
+    axes.tick_params(
+        axis="both",
+        which="both",
+        direction="out",
+        length=5.0,
+        width=3.0,
+        color=colors["black"],
+        pad=5,
+        labelsize=fonts["values"]["one"]["size"],
+        labelcolor=colors["black"]
+    )
+# Plot points for values from each group.
+    index = 0
+    for name, group in groups:
+        values_x = group[abscissa].to_list()
+        values_y = group[ordinate].to_list()
+        handle = axes.plot(
+            values_x,
+            values_y,
+linestyle="",
+marker= shape_series[index],
+markersize=9, 
+markeredgecolor=colors["black"],
+markerfacecolor=colors_series[index] # colors["black"]
+        )
+        index += 1
+        pass
+# Plot labels for each group.
+    labels = []
+    index = 0
+    for name, group in groups:
+        values_x = group[abscissa].to_list()
+        mean_x = statistics.median(values_x)
+        values_y = group[ordinate].to_list()
+        mean_y = statistics.median(values_y)
+#        axes.text(
+#            mean_x,
+#            mean_y,
+#str(index+1),
+#backgroundcolor=colors["white_faint"],
+#color=colors["black"],
+#fontproperties=fonts["properties"]["three"],
+#horizontalalignment="center",
+#verticalalignment="center"
+#        )
+        label = str(index+1) + ": " + name
+        labels.append(label)
+        index += 1
+        pass
+# Create legend.
+# Create custome elements for the legend
+    elements = create_legend_elements(
+colors=colors_series,
+markers= shape_series,
+labels=labels
+    )
+    axes.legend(
+handles=elements,
+loc="lower right",
+prop=fonts["properties"]["four"],
+    )
+    return figure
+
+
