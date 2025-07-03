11168597261e96c9250d76fdacc8725cadcef01e
Lucia Bazan-Williamson <jbazanwilliamson@ucmerced.edu>
2025-07-03 16:56:53 -0700

    updates_figures

--- DAPC-hprc.R
+++ DAPC-hprc.R
@@ -1,17 +1,28 @@
 library("adegenet")
+library('stats')
+library("hierfstat")
+library("devtools")
+library("pegas")
 
 myCol <- c("darkblue","purple","green","orange","red","blue")
 #T2T data
 data_t2t <- read.csv('/Users/luciabazan/Documents/GitHub/cnv_sgdp/data/input_dapc_t2t_hprc.csv')
+#names(data_t2t)[2] <- "population"
 pop <- data_t2t$Superpopulation
 data_t2t <- data_t2t[ -c(1,2) ]
-dapc.data_t2t <-  dapc(data_t2t, grp=pop, var.contrib = TRUE, scale = FALSE, n.pca = 6, n.da = 100)
+dapc.data_t2t <-  dapc(data_t2t, grp=pop, var.contrib = TRUE, scale = FALSE, n.pca = 11, n.da = 100)
+
+## Optimal PCs from a-score 
+temp <- optim.a.score(dapc.data_t2t)
+
 
 scatter(dapc.data_t2t, pch = 18:23, cstar = 0,  scree.da=FALSE,  leg=TRUE, clab=0, main = 'T2T')
 # Centroids of every population
 centroids_t2t = dapc.data_t2t[["grp.coord"]]
 write.csv(centroids_t2t,"/Users/luciabazan/Documents/GitHub/cnv_sgdp/data/centroids_t2t.csv")
-## 
+
+
+
 #scatter(dapc.data_t2t, pch = 18:23, cstar = 0, mstree = TRUE, lwd = 2, lty = 2, scree.da=TRUE, posi.da="bottomleft", leg=TRUE, clab=0)
 
 scatter(dapc.data_t2t, posi.da="bottomright",  bg="white",
@@ -26,7 +37,7 @@ compoplot(dapc.data_t2t, lab="", posi=list(x=12,y=-.01), cleg=.7, space=(0), mai
 
 
 ## Barplot
-temp <- summary(dapc(data_t2t, grp=pop, var.contrib = TRUE, scale = FALSE, n.pca = 5, n.da = 6))$assign.per.pop*100
+temp <- summary(dapc(data_t2t, grp=pop, var.contrib = TRUE, scale = FALSE, n.pca = 6, n.da = 100))$assign.per.pop*100
 par(mar=c(4.5,7.5,1,1))
 barplot(temp, xlab="% of reassignment to actual population",
         horiz=TRUE, las=1, main='T2T')
@@ -36,6 +47,9 @@ barplot(temp, xlab="% of reassignment to actual population",
 xval <- xvalDapc(data_t2t, pop, n.pca.max = 11, training.set = 0.9,
                  result = "groupMean", center = TRUE, scale = FALSE,
                  n.pca = NULL, n.rep = 30, xval.plot = TRUE)
+
+
+xval
 
 #############GRCh37 data
 data_grch37 <- read.csv('/Users/luciabazan/Documents/GitHub/cnv_sgdp/data/input_dapc_grch38_hprc.csv')
@@ -52,7 +66,7 @@ scatter(dapc.data_grch37, pch = 18:23, cstar = 0, scree.da=FALSE, leg=TRUE, clab
 scatter(dapc.data_grch37, cell = 0, pch = 18:23, cstar = 0, mstree = TRUE, lwd = 2, lty = 2, scree.da=TRUE, posi.da="bottomleft", leg=TRUE, clab=0)
 
 
-## 
+## STRUCTURE LIKE MAP
 assignplot(dapc.data_grch37)
 
 par(mar=c(5.1,4.1,1.1,1.1), xpd=TRUE)
